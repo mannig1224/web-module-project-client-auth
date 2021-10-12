b393af9de245ac885cf4d844bfd07dcf3c6346e3
ManniG24 <egatica51@gmail.com>
2021-10-12 13:36:25 -0600

    started login component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Login from './components/Login'
+
 function App() {
+
+
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+      </ul>
+      <Switch>
+          
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Login} />    
+        </Switch>
+
     </div>
+
+    </Router>
+    
   );
 }
 
